5a49089debc9431c28a734b111285708fa2466dd
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-02-01 11:25:11 +0100

    added clear function and canvas attribute to world

--- components/classes/world.class.js
+++ components/classes/world.class.js
@@ -5,16 +5,22 @@ class World {
         new Pufferfish(),
         new Pufferfish()
     ];
+    canvas;
     ctx;
     constructor (canvas) {
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
     draw(){
-        this.ctx.clearReact(0,0, this.canvas.width, this.canvas.height);
+        this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
 
         this.ctx.drawImage(this.hero.image, this.hero.x, this.hero.y, this.hero.width, this.hero.height);
+
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.image, enemy.x, enemy.y, enemy.width, enemy.height);
+        });
 
         self = this;
         requestAnimationFrame( function () {
