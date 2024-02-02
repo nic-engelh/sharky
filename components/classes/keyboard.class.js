3a689fb828a4ed9c13072f5a18da79a5839e0689
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-02-02 15:26:29 +0100

    further changes to keyboard listener

--- components/classes/keyboard.class.js
+++ components/classes/keyboard.class.js
@@ -4,8 +4,19 @@ class Keyboard {
     up = false;
     down = false;
     space = false;
+    enter = false;
+    escape = false;
 
     constructor(){
-        
     };
+
+    reset() {
+        left = false;
+        right = false;
+        up = false;
+        down = false;
+        space = false;
+        enter = false;
+        escape = false;
+    }
 }
--- game.js
+++ game.js
@@ -10,27 +10,24 @@ window.addEventListener("keydown", (event) => {
     console.log(event);
     pressedKey = event.code;
     if (pressedKey == "ArrowDown") {
-
+        this.keyboard.down = true;
     } 
     if (pressedKey == "ArrowRight") {
-
+        this.keyboard.right = true;
     }
     if (pressedKey == "ArrowLeft") {
-
+        this.keyboard.left = true;
     }
     if (pressedKey == "ArrowUp") {
-
+        this.keyboard.up = true;
     }
     if (pressedKey == "Enter") {
-
+        this.keyboard.enter = true;
     }
     if (pressedKey == "Space") {
-
+        this.keyboard.space = true;
     }
     if (pressedKey == "Escape") {
-
+        this.keyboard.escape = true;
     }
-
-
-
 } );
