7132ada857eaf51a5f50c2546be694934e9a90fb
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-02-02 11:15:20 +0100

    added anmiate function for hero

--- components/classes/hero.class.js
+++ components/classes/hero.class.js
@@ -1,7 +1,27 @@
 class Hero extends MovableObject {
+    currentImage = 0;
+    imagesWalking = [
+        '/assets/img/1.Sharkie/3.Swim/1.png',
+        '/assets/img/1.Sharkie/3.Swim/2.png',
+        '/assets/img/1.Sharkie/3.Swim/3.png',
+        '/assets/img/1.Sharkie/3.Swim/4.png',
+        '/assets/img/1.Sharkie/3.Swim/5.png',
+        '/assets/img/1.Sharkie/3.Swim/6.png'
+    ]
 
     constructor () {
         super().loadImage('/assets/img/1.Sharkie/3.Swim/1.png');
+        this.loadImages(this.imagesWalking);
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let path = this.imagesWalking[this.currentImage];
+            this.image = this.imageCache[path];
+            this.currentImage++;
+        }, 1000);
+       
     }
 
 
--- components/classes/movableObject.class.js
+++ components/classes/movableObject.class.js
@@ -4,11 +4,21 @@ class MovableObject {
     height = 245;
     width = 300;
     image;
+    imageCache = new Map();
 
     loadImage (path) {
         this.image = new Image();
         this.image.src = path;
     }
+
+    loadImages (images) {
+        images.forEach(imagePath => {
+            let img = new Image();
+            img.src = imagePath;
+            this.imageCache.set(imagePath, img);
+        });
+    }
+
     moveRight(){
         return true
     };
