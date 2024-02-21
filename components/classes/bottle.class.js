df36c7a9f64f122ec95c5d223d8353d3e95103b7
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-02-21 15:44:27 +0100

    fixing

--- components/classes/bottle.class.js
+++ components/classes/bottle.class.js
@@ -8,14 +8,14 @@ class Bottle extends MovableObject {
         '/assets/img/4. Marcadores/Posion/Animada/5.png',
         '/assets/img/4. Marcadores/Posion/Animada/6.png',
         '/assets/img/4. Marcadores/Posion/Animada/7.png',
-        '/assets/img/4. Marcadores/Posion/Animada/8.png',
+        '/assets/img/4. Marcadores/Posion/Animada/8.png'
     ];
     
     constructor () {
         super().loadImage('/assets/img/4. Marcadores/Posion/Animada/1.png');
         this.loadImages(this.imagesMoving);
         this.width = 90;
-        this.heigt = 120;
+        this.height = 120;
         this.x = 400 + Math.random() * 2000;
         this.y = 300 - Math.random() * 300;
         this.offSetTop = 0;
