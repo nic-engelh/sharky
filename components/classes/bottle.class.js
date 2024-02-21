170be6b3657dddeeb26c6bcc5f77c20336711df4
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-02-21 15:21:07 +0100

    changed img path

--- components/classes/bottle.class.js
+++ components/classes/bottle.class.js
@@ -1,22 +1,22 @@
 class Bottle extends MovableObject {
 
     imagesMoving = [
-        '/assets/img/4. Marcadores/Posiขn/Animada/1.png',
-        '/assets/img/4. Marcadores/Posiขn/Animada/2.png',
-        '/assets/img/4. Marcadores/Posiขn/Animada/3.png',
-        '/assets/img/4. Marcadores/Posiขn/Animada/4.png',
-        '/assets/img/4. Marcadores/Posiขn/Animada/5.png',
-        '/assets/img/4. Marcadores/Posiขn/Animada/6.png',
-        '/assets/img/4. Marcadores/Posiขn/Animada/7.png',
-        '/assets/img/4. Marcadores/Posiขn/Animada/8.png',
+        '/assets/img/4. Marcadores/Posion/Animada/1.png',
+        '/assets/img/4. Marcadores/Posion/Animada/2.png',
+        '/assets/img/4. Marcadores/Posion/Animada/3.png',
+        '/assets/img/4. Marcadores/Posion/Animada/4.png',
+        '/assets/img/4. Marcadores/Posion/Animada/5.png',
+        '/assets/img/4. Marcadores/Posion/Animada/6.png',
+        '/assets/img/4. Marcadores/Posion/Animada/7.png',
+        '/assets/img/4. Marcadores/Posion/Animada/8.png',
     ];
     
     constructor () {
-        super().loadImage('/assets/img/4. Marcadores/Posiขn/Animada/1.png');
+        super().loadImage('/assets/img/4. Marcadores/Posion/Animada/1.png');
         this.loadImages(this.imagesMoving);
         this.width = 90;
         this.heigt = 120;
-        this.x = 500 + Math.random() * 2000;
+        this.x = 400 + Math.random() * 2000;
         this.y = 300 - Math.random() * 300;
         this.offSetTop = 0;
         this.offsetBottom = 0;
