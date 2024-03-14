e2666af09e0866f7d7b4ebb6887f3b4426023363
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-03-14 07:44:14 +0100

    Added functions for display orientation checking

--- components/utils/script.js
+++ components/utils/script.js
@@ -1,30 +1,31 @@
+document.getElementById("btn-start").addEventListener("click", hideButton);
+const impressumButton = document.getElementById("button-impressum");
+const impressumDialog = document.getElementById("dialog-impressum");
+const impressumText = document.getElementById("dialog-impressum-text-box");
+
 function hideButton() {
-    const btnStart = document.getElementById("btn-start");
-    btnStart.style.display = "none";
+  const btnStart = document.getElementById("btn-start");
+  btnStart.style.display = "none";
 }
 
 function resetOpacity() {
-    const canvas = document.getElementById("canvas");
-    canvas.classList.remove("opaque");
+  const canvas = document.getElementById("canvas");
+  canvas.classList.remove("opaque");
 }
 
-document.getElementById("btn-start").addEventListener("click", hideButton);
-
-
-
 function checkOrientation() {
-    const dialogElement = document.getElementById("dialog-device-orientation");
-    if (!dialogElement) return; // Wenn das Dialog-Element nicht gefunden wurde, die Funktion abbrechen
+  const dialogElement = document.getElementById("dialog-device-orientation");
+  if (!dialogElement) return; // Wenn das Dialog-Element nicht gefunden wurde, die Funktion abbrechen
 
-    if (window.matchMedia("(orientation: portrait)").matches) {
-        // Wenn das Gerät im Hochformat ist
-        dialogElement.showModal(); // Dialog anzeigen
-    } else {
-        // Wenn das Gerät im Querformat ist
-        if (typeof dialogElement.close === 'function') {
-            dialogElement.close(); // Dialog schließen, falls close() verfügbar ist
-        }
+  if (window.matchMedia("(orientation: portrait)").matches) {
+    // Wenn das Gerät im Hochformat ist
+    dialogElement.showModal(); // Dialog anzeigen
+  } else {
+    // Wenn das Gerät im Querformat ist
+    if (typeof dialogElement.close === "function") {
+      dialogElement.close(); // Dialog schließen, falls close() verfügbar ist
     }
+  }
 }
 
 // Die Ausrichtung überprüfen, wenn sich die Bildschirmgröße ändert oder das Gerät gedreht wird
@@ -32,23 +33,16 @@ window.addEventListener("resize", checkOrientation);
 window.addEventListener("orientationchange", checkOrientation);
 
 // Überprüfen Sie die Ausrichtung, wenn die Seite geladen wird
-window.onload = function() {
-    checkOrientation();
+window.onload = function () {
+  checkOrientation();
 };
 
-
-
-const impressumButton = document.getElementById("button-impressum");
-const impressumDialog = document.getElementById("dialog-impressum");
-const impressumText = document.getElementById("dialog-impressum-text-box");
-
-impressumButton.addEventListener("click", function() {
-    impressumDialog.showModal();
-    impressumText.innerHTML = impressumHTML();
-
+impressumButton.addEventListener("click", function () {
+  impressumDialog.showModal();
+  impressumText.innerHTML = impressumHTML();
 });
 
 function closeImpressumDialog() {
-    impressumDialog.close();
-    impressumText.innerHTML = "";
+  impressumDialog.close();
+  impressumText.innerHTML = "";
 }
