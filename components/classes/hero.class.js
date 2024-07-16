7fcb8cafc7cd9f69c70a1efccd33d1bcd72beadd
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-07-16 14:56:02 +0200

    bug fixing

--- components/classes/hero.class.js
+++ components/classes/hero.class.js
@@ -1,11 +1,11 @@
 class Hero extends MovableObject {
   imagesWalking = [
-    "../../assets/img/1.Sharkie/3.Swim/1.png",
-    "../../assets/img/1.Sharkie/3.Swim/2.png",
-    "../../assets/img/1.Sharkie/3.Swim/3.png",
-    "../../assets/img/1.Sharkie/3.Swim/4.png",
-    "../../assets/img/1.Sharkie/3.Swim/5.png",
-    "../../assets/img/1.Sharkie/3.Swim/6.png",
+    "https://niklas-engelhardt.de/sharky/assets/img/1.Sharkie/3.Swim/1.png",
+    "https://niklas-engelhardt.de/sharky/assets/img/1.Sharkie/3.Swim/2.png",
+    "https://niklas-engelhardt.de/sharky/assets/img/1.Sharkie/3.Swim/3.png",
+    "https://niklas-engelhardt.de/sharky/assets/img/1.Sharkie/3.Swim/4.png",
+    "https://niklas-engelhardt.de/sharky/assets/img/1.Sharkie/3.Swim/5.png",
+    "https://niklas-engelhardt.de/sharky/assets/img/1.Sharkie/3.Swim/6.png",
   ];
 
   imagesWaiting = [
