1773c5b3de5c7acf2e51ab4f10cc3f1e814f27e2
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-07-16 13:55:07 +0200

    changed paths audio

--- components/classes/audioManager.class.js
+++ components/classes/audioManager.class.js
@@ -11,50 +11,50 @@ class AudioManager {
    */
   loadSounds() {
     this.sounds["waterflowEffect"] = new Audio(
-      "../../assets/sounds/underwater-loop-amb-6182.mp3"
+      "./sharky/assets/sounds/underwater-loop-amb-6182.mp3"
     );
     this.sounds["ambient"] = new Audio(
-      "../../assets/sounds/027210_39danger-and-beauty39-ukulele-loop-by-reamp3-71291.mp3"
+      "./sharky/assets/sounds/027210_39danger-and-beauty39-ukulele-loop-by-reamp3-71291.mp3"
     );
     this.sounds["bottle"] = new Audio("../../assets/sounds/bottle-pop-45531.mp3");
     this.sounds["coin"] = new Audio(
-      "../../assets/sounds/short-success-sound-glockenspiel-treasure-video-game-6346.mp3"
+      "./sharky/assets/sounds/short-success-sound-glockenspiel-treasure-video-game-6346.mp3"
     );
     this.sounds["heroHurt"] = new Audio(
-      "../../assets/sounds/male-hurt-sound-95206.mp3"
+      "./sharky/assets/sounds/male-hurt-sound-95206.mp3"
     );
     this.sounds["introSound"] = new Audio(
-      "../../assets/sounds/evil-laugh-45966.mp3"
+      "./sharky/assets/sounds/evil-laugh-45966.mp3"
     );
     this.sounds["ambientBoss"] = new Audio(
-      "../../assets/sounds/tribal-loop-azteca-154482.mp3"
+      "./sharky/assets/sounds/tribal-loop-azteca-154482.mp3"
     );
     this.sounds["biteSounds"] = new Audio(
-      "../../assets/sounds/monster-bite-44538.mp3"
+      "./sharky/assets/sounds/monster-bite-44538.mp3"
     );
     this.sounds["winSound"] = new Audio(
-      "../../assets/sounds/you-win-sequence-2-183949.mp3"
+      "./sharky/assets/sounds/you-win-sequence-2-183949.mp3"
     );
     this.sounds["woundingSound"] = new Audio(
-      "../../assets/sounds/giant-breath-1-184041.mp3"
+      "./sharky/assets/sounds/giant-breath-1-184041.mp3"
     );
     this.sounds["swimming"] = new Audio(
-      "../../assets/sounds/fish-in-river-6114.mp3"
+      "./sharky/assets/sounds/fish-in-river-6114.mp3"
     );
     this.sounds["bubbleShot"] = new Audio(
-      "../../assets/sounds/sfx_submerge-14424.mp3"
+      "./sharky/assets/sounds/sfx_submerge-14424.mp3"
     );
     this.sounds["finSlap"] = new Audio(
-      "../../assets/sounds/indiana-jones-style-punchwav-14551.mp3"
+      "./sharky/assets/sounds/indiana-jones-style-punchwav-14551.mp3"
     );
     this.sounds["enemyBubblePop"] = new Audio(
-      "../../assets/sounds/balloonpop-83760.mp3"
+      "./sharky/assets/sounds/balloonpop-83760.mp3"
     );
     this.sounds["shocked"] = new Audio(
-      "../../assets/sounds/electric_zap_001-6374.mp3"
+      "./sharky/assets/sounds/electric_zap_001-6374.mp3"
     );
     this.sounds["transform"] = new Audio(
-      "../../assets/sounds/balloon-inflate-1-184052.mp3"
+      "./sharky/assets/sounds/balloon-inflate-1-184052.mp3"
     );
   }
 
